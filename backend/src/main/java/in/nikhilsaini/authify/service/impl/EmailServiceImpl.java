1d5d3d18fbd24bc719207cd1dca2aa6cc653083f
Nikhil <saininikhil2911@gmail.com>
2025-12-04 15:36:38 +0530

    solved email error

--- backend/src/main/java/in/nikhilsaini/authify/service/impl/EmailServiceImpl.java
+++ backend/src/main/java/in/nikhilsaini/authify/service/impl/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package in.nikhilsaini.authify.service.impl;
 
 import in.nikhilsaini.authify.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -11,32 +12,37 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     private final JavaMailSender mailSender;
-    private final  String APP_NAME = "Authify";
+    private final String APP_NAME = "Authify";
 
     @Async
     @Override
     public void sendVerificationOtp(String toEmail, String otp) {
         String subject = "Verify your email-" + APP_NAME;
-        String body = "Your verification OTP is: " + otp + "\n\n"+ "this OTP will expire in 10  minutes.\n"
-                +"Thank you for registrating with "+ APP_NAME+ "!";
+        String body = "Your verification OTP is: " + otp + "\n\n" + "this OTP will expire in 10  minutes.\n"
+                + "Thank you for registrating with " + APP_NAME + "!";
 
-        sendEmail(toEmail,subject,body);
+        sendEmail(toEmail, subject, body);
 
     }
+
     @Async
     @Override
     public void sendPasswordResetOtp(String toEmail, String otp) {
         String subject = "Reset your Password -" + APP_NAME;
-        String body= "Your password reset OTP is: " + otp + "\n\n"
+        String body = "Your password reset OTP is: " + otp + "\n\n"
                 + "This OTP will expire in 10 minutes.\n"
                 + "If you did not request this, please ignore this email.";
-        sendEmail(toEmail,subject,body);
+        sendEmail(toEmail, subject, body);
 
     }
+
     @Async
     @Override
-    public void sendWelcomeEmail(String toEmail, String name){
+    public void sendWelcomeEmail(String toEmail, String name) {
         String subject = "Welcome to " + APP_NAME + "!";
         String body = "Hello " + name + ",\n\n"
                 + "Your email has been successfully verified.\n"
@@ -52,16 +58,24 @@ public class EmailServiceImpl implements EmailService {
     // Private Helper Method
     // --------------------------------
 
-    private void sendEmail(String to, String subjet, String body)
+    private void sendEmail(String to, String subjet, String body) {
+    try
     {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subjet);
         message.setText(body);
-        message.setFrom("no-reply@authify.com");
+        message.setFrom(fromEmail);
 
         mailSender.send(message);
+        System.out.println("email sent to" + to);
+    } catch(Exception e)
+
+    {
+        System.out.println("Email failed to send!");
+        e.printStackTrace();
     }
+}
 
 
 }
